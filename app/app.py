fd2fb2f1a3eab109134f7ab76f4c93c3cf63cf2f
Tony Jurg <114746162+tonyjurg@users.noreply.github.com>
2025-03-06 13:30:16 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,13 @@
 from tf.advanced.app import App
 from tf.advanced.display import displaySetup, displayReset
+from tf.advanced.search import search
 
+class TfApp(App):
+
+    def run_queries(app,query):
+        results = search(app, query)
+        return results
+
+    def __init__(app, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        app.dm('method `run_queries` made available')
